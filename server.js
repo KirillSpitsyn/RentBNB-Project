b25e710ee4bec9ad49274b9a4e34c211c3f5eb23
Kirill Spitsyn <spitsyn@myseneca.ca>
2020-10-29 23:34:39 -0400

    new update

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-const hbs = require('express-handlebars');
+
 var nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
 var HTTP_PORT = process.env.PORT || 8080;
 
-app.engine('.hbs', hbs({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,12 +21,12 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 app.get("/dashboard", function(req,res){
-    res.render('dashboard',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/dashboard.html"));
 });
 app.post("/dashboard", function(req,res){
     const FORM_DATA = req.body;
 DATA_OUTPUT="<h1>Your have successfully registered!</h1> <br/><br/>" +
-       "<h3">Welcome <strong> + FORM_DATA.firstname + " " + FORM_DATA.lastname + "</strong> to the Canada Rentals.</h3>";
+       "<h3>Welcome <strong>" + FORM_DATA.firstname + " " + FORM_DATA.lastname + "</strong> to the Canada Rentals.</h3>";
     res.send(DATA_OUTPUT);
     var emailOptions = {
         from: 'zima197211@gmail.com',
@@ -47,30 +46,27 @@ DATA_OUTPUT="<h1>Your have successfully registered!</h1> <br/><br/>" +
 });
 
 app.get("/", function(req,res){
-    res.render('index.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.get("/register", function(req,res){ 
-    res.render('register.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 });
-
 app.get("/upload", function(req,res){ 
-    res.render('uploadPage.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/uploadPage.html"));
 });
 app.get("/rooms", function(req,res){ 
-    res.render('roomListings.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/roomListings.html"));
 });
 app.get("/details", function(req,res){ 
-    res.render('detailsPage.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/detailsPage.html"));
 });
 app.get("/signin", function(req,res){ 
-    res.render('signin.hbs',{layout: false});
+    res.sendFile(path.join(__dirname, "/views/signin.html"));
 });
 
 
 app.use(express.static('views'));
 app.use(express.static("public"));
-
-
 
 app.listen(HTTP_PORT, onHttpStart);
